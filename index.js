3dfc9b53a3e347c27a2b46009fd8b2d86829ae5a
Bharat Dua <bharatdua13@gmail.com>
2024-05-03 14:47:24 +0530

    DB connect successfully

--- index.js
+++ index.js
@@ -4,6 +4,12 @@ const mongoose = require("mongoose");
 // init app
 const app = express();
 
+// connect to MongoDB database
+const connectionUrl = "mongodb://0.0.0.0:27017/todoDb";
+mongoose
+  .connect(connectionUrl)
+  .then(() => console.log("Database connection successful"))
+  .catch((err) => console.log(err.message));
 // view engine
 app.set("view engine", "ejs");
 
